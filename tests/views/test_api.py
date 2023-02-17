bfe4c99f88df1efe938ef56882ba4222c8b8d1a8
Israel Silva <israel.bernardes.silva@gmail.com>
2023-02-17 19:19:02 -0300

    feat: add routes integration with algorithms

--- tests/views/test_api.py
+++ tests/views/test_api.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import time
+from http import HTTPStatus
 
 import pytest
 import requests
@@ -27,13 +28,20 @@ def start_server():
 
 def test_vowel_count_ok():
     url = f'http://{HOST}:{PORT}/vowel_count'
-    response = requests.post(url, json={'vowel_count': 'test'})
-    assert response.status_code == 200
-    assert response.text == '{"vowel_count": "test"}'
+    response = requests.post(url, json={'words': ['batman', 'robin', 'coringa']})
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '{"batman": 2, "robin": 2, "coringa": 3}'
 
 
-def test_sort_ok():
+def test_sort_asc_ok():
     url = f'http://{HOST}:{PORT}/sort'
-    response = requests.post(url, json={'sort': 'test'})
-    assert response.status_code == 200
-    assert response.text == '{"sort": "test"}'
+    response = requests.post(url, json={'words': ['batman', 'robin', 'coringa'], 'order': 'asc'})
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '["batman", "coringa", "robin"]'
+
+
+def test_sort_desc_ok():
+    url = f'http://{HOST}:{PORT}/sort'
+    response = requests.post(url, json={'words': ['batman', 'robin', 'coringa'], 'order': 'desc'})
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '["robin", "coringa", "batman"]'
--- views/api.py
+++ views/api.py
@@ -2,6 +2,9 @@ import json
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
 
+from algorithms.vowel_count import get_vowel_count
+from algorithms.sort import get_sorted_list
+
 
 class ApiViews(BaseHTTPRequestHandler):
 
@@ -9,19 +12,41 @@ class ApiViews(BaseHTTPRequestHandler):
         payload = self.get_payload()
 
         if self.path == '/vowel_count':
-            response = json.dumps(payload).encode(encoding='utf_8')
-            status_code = HTTPStatus.OK
+            response, status_code = self.create_response_vowel_count(payload)
 
         if self.path == '/sort':
-            response = json.dumps(payload).encode(encoding='utf_8')
-            status_code = HTTPStatus.OK
+            response, status_code = self.create_response_sort(payload)
 
         self.send_response(status_code)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        self.wfile.write(response)
+        self.wfile.write(json.dumps(response).encode(encoding='utf_8'))
 
     def get_payload(self) -> json:
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         return json.loads(body)
+
+    @staticmethod
+    def create_response_vowel_count(payload: json) -> tuple[dict, HTTPStatus]:
+        response = {}
+        for word in payload.get('words'):
+            response[word] = get_vowel_count(word)
+        status_code = HTTPStatus.OK
+
+        return response, status_code
+
+    @staticmethod
+    def create_response_sort(payload: json) -> tuple[list, HTTPStatus]:
+        map_order_reverse_list = {
+            'asc': False,
+            'desc': True
+        }
+
+        words = payload.get('words')
+        order = payload.get('order')
+
+        sorted_list = get_sorted_list(words, reverse=map_order_reverse_list[order])
+        status_code = HTTPStatus.OK
+
+        return sorted_list, status_code
